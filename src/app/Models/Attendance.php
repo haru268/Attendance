6bd4df6eafd212dda095eba133c71ea520a931c7
haru268 <ha3110rukiruki@gmail.com>
2025-08-16 10:04:04 +0900

    1

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -21,8 +21,8 @@ class Attendance extends Model
 
     protected $casts = [
         'date'      => 'date',         
-        'clock_in'  => 'datetime:H:i', 
-        'clock_out' => 'datetime:H:i',
+        'clock_in'  => 'datetime', 
+        'clock_out' => 'datetime',
     ];
 
 
